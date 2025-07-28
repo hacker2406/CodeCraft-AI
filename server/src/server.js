c291e45eced613d885626a43c95461e10d32f4d1
hacker2406 <ratul.9paul@gmail.com>
2025-07-29 02:05:59 +0530

    small slash removed causing cors error

--- server/src/server.js
+++ server/src/server.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://code-craft-ai-zeta.vercel.app/"
+      "https://code-craft-ai-zeta.vercel.app"
     ], 
     credentials: true,
   })
