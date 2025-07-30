2fb5d51f88ad9253086c28e2bbe061e94cad93b2
hacker2406 <ratul.9paul@gmail.com>
2025-07-30 21:08:23 +0530

    tailwind changes

---
+++ client/tailwind.config.mjs
@@ -0,0 +1,32 @@
+// tailwind.config.mjs
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  // Add this safelist to solve the react-live issue
+  safelist: [
+    {
+      pattern: /^(bg|text|border|ring)-(red|green|blue|yellow|purple|gray|indigo|pink|white|black)-(100|200|300|400|500|600|700|800|900)$/,
+    },
+    {
+      pattern: /^(p|px|py|pt|pr|pb|pl|m|mx|my|mt|mr|mb|ml)-[0-9]+$/,
+    },
+    {
+      pattern: /^(w|h)-(full|screen|[0-9/]+)$/,
+    },
+    {
+      pattern: /^(flex|grid|items|justify|gap|rounded)-.+$/,
+    },
+  ],
+
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
